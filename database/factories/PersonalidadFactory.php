28857919f93d2dceea39f97df32c2a3c8ee2277d
=Fredy Rojas <=farojas851@gmail.com>
2019-10-09 16:20:14 -0500

    Migracion, Modelo y Controlador para Personalidad

---
+++ database/factories/PersonalidadFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Personalidad;
+use Faker\Generator as Faker;
+
+$factory->define(Personalidad::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
